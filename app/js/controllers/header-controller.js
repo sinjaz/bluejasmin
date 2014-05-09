7e91bdeaac7070ec803494adf2ad6d83081eab61
Sina Jazayeri <sin.jaz@gmail.com>
2014-05-08 23:40:00 -0400

    Added header

--- app/js/controllers/header-controller.js
+++ app/js/controllers/header-controller.js
@@ -10,14 +10,39 @@ sinjaz.controllers.controller('HeaderController', ['$scope', '$location', '$rout
         [
         	{
                 id : 0,
-        		buttonTitle : 'Work & Serious',
+        		buttonTitle : 'Our Approach',
                 url : '#/me/work'
         	},
         	{
                 id : 1,
-        		buttonTitle: 'Fun & Play!!',
+        		buttonTitle: 'Advertisers',
                 url : '#/me/fun'
-        	}
+        	},
+            {
+                id : 1,
+                buttonTitle: 'Create Ad Gallery',
+                url : '#/me/fun'
+            },
+            {
+                id : 1,
+                buttonTitle: 'Publishers',
+                url : '#/me/fun'
+            },
+            {
+                id : 1,
+                buttonTitle: 'Blog',
+                url : '#/me/fun'
+            },
+            {
+                id : 1,
+                buttonTitle: 'Contact Us',
+                url : '#/me/fun'
+            },
+            {
+                id : 1,
+                buttonTitle: 'Job Postings',
+                url : '#/me/fun'
+            }
         ];
   }]);
 
